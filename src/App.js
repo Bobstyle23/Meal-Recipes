2de98fcac9fe0bef4a920dfd6ad8168388646044
Bob Pakhriev <boburpakhriev@gmail.com>
2021-07-31 14:22:59 +0900

    add some styles

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import axios from "axios";
 
 const appId = "3681a864";
 const appKey = "e69c2c126b0e1f9d6bd7e7a5e90d78f9";
-const url = `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${appId}&app_key=${appKey}`;
 
 function App() {
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
+
   useEffect(() => {
     getRecipe();
-  }, []);
+  }, [query]);
 
   // const getRecipes = async () => {
   //   const response = await fetch(url);
@@ -19,23 +21,51 @@ function App() {
   // };
 
   const getRecipe = async () => {
-    const data = await axios.get(url);
+    const data = await axios.get(
+      `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${appId}&app_key=${appKey}`
+    );
     setRecipes(data.data.hits);
-    console.log(data);
   };
 
+  const searchHandler = (event) => {
+    setSearch(event.target.value);
+    //for input
+  };
+
+  const getSearch = (event) => {
+    event.preventDefault();
+    setQuery(search);
+    setSearch("");
+    //for form submission
+  };
   return (
     <div className="content">
-      {recipes.map((recipe) => (
-        <Recipes
-          key={recipe.recipe.image}
-          title={recipe.recipe.label}
-          image={recipe.recipe.image}
-          calories={Math.floor(recipe.recipe.calories) * 1 + " cal"}
-          cuisine={recipe.recipe.cuisineType}
-          labels={recipe.recipe.dietLabels}
+      <form className="search-form" onSubmit={getSearch}>
+        <input
+          type="text"
+          onChange={searchHandler}
+          className="search-bar"
+          value={search}
+          placeholder="Search for recipes..."
         />
-      ))}
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+
+      <div className="recipes">
+        {recipes.map((recipe) => (
+          <Recipes
+            key={recipe.recipe.image}
+            title={recipe.recipe.label}
+            image={recipe.recipe.image}
+            calories={Math.floor(recipe.recipe.calories) * 1 + " cal"}
+            cuisine={recipe.recipe.cuisineType}
+            labels={recipe.recipe.dietLabels}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import style from "../recipe.module.css";
 
-const Recipes = ({ title, image, calories, cuisine, labels }) => {
+const Recipes = ({ title, image, calories, cuisine, labels, ingredients }) => {
   return (
-    <div>
-      <h1>{title}</h1>
+    <div className={style.recipe}>
+      <h3 className={style.header}>{title}</h3>
+      <ul>
+        {ingredients.map((ingredient) => (
+          <li>{ingredient.text}</li>
+        ))}
+      </ul>
       <p>{calories}</p>
       <h5>Cuisine Type: {cuisine}</h5>
       <p>Diet Labels: {labels}</p>
-      <img src={image} alt="" />
+      <img className="image" src={image} alt={title} />
     </div>
   );
 };
